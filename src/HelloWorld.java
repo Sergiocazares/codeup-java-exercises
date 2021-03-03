6defabc10a69462188a57afc35cfcb6c43c60208
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-03-03 12:11:05 -0600

    finished 1-3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,12 @@ public class HelloWorld {
         //        System.out.println("Hello, World!");
         System.out.print("Hello,");
         System.out.print(" World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Codeup Exercises!";
+        System.out.println(myString);
     }
 
 }
