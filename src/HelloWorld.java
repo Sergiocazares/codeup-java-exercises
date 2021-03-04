e8683d58668faba16d9af5015d7df7afffca66a1
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-03-04 13:35:41 -0600

    completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,68 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         //        System.out.println("Hello, World!");
-        System.out.print("Hello,");
-        System.out.print(" World!");
+//        System.out.print("Hello,");
+//        System.out.print(" World!");
+//
+//        int myFavoriteNumber = 13;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Codeup Exercises!";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//
+//        int y = 5;
+//        System.out.println(y++);
+//        System.out.println(y);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String class = "Can't do that";
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+//
+//        System.out.println(three);
+//
+//        int x = 4;
+//        x += 5;
+//
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+//
+//       byte bitesizeNum = -128;
+//       System.out.println(--bitesizeNum);
+//       System.out.println(bitesizeNum - 1);
+//       short shortNum = -32768;
+//       System.out.println(--shortNum);
+//       System.out.println(shortNum - 1);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter something :");
+//
+//        String userInput = scanner.next();
+//
+//        System.out.printf("You entered: --> %s <--", userInput);
 
-        int myFavoriteNumber = 13;
-        System.out.println(myFavoriteNumber);
-
-        String myString = "Codeup Exercises!";
-        System.out.println(myString);
     }
-
 }
